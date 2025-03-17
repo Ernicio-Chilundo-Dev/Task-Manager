05c9595cf54fd3c1c0e877edff51c80ba9612318
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-17 20:58:21 +0200

    add

--- task.cpp
+++ task.cpp
@@ -31,17 +31,37 @@ void adicionarTarefas(){
 // funcao para listar todas as tarefas
 void  listarTarefa(){
     if(listaDeTarefas.empty()){
-        cout << "Nenhuma tarefa registada!\n\n ";
+        cout << "Nenhuma tarefa registrada!\n\n ";
         return;
     }
 
     cout << "===Lista de Tarefas===\n";
     for(const auto& tarefa:listaDeTarefas){
-        cout<< "ID: "<<tarefa.id<<" - "<<"Descricao: "<<tarefa.descricao<<endl;
+        cout<< "ID: "<<tarefa.id<<" - "<<tarefa.descricao<<endl;
     }
     cout<<endl;
 }
 
+//Funcao para remover uma tarefa pelo ID
+void removerTarefa(){
+    if(listaDeTarefas.empty()){
+        cout  << "Nao ha tarefas para remover\n\n";
+        return;
+    }
+
+    int id;
+    cout << "Digite o ID da tarefa a ser removida\n";
+     cin >> id;
+    for(auto it  = listaDeTarefas.begin();it!=listaDeTarefas.end();++it){
+        if(it->id==id){
+            listaDeTarefas.erase(it);
+            cout<<"Tarefa removida com sucesso!\n\n";
+            return;
+        }
+    }
+
+    cout <<"Tarefa com ID "<<id<<" nao encontrada!";
+}
 int maian(){
 
     return 0;
