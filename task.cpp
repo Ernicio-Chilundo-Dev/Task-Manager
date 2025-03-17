3e6961bda9b4ba51c0bd1f1c785214c4a651f535
Ernicio-Chilundo-Dev <ernicio.j.chilundo@gmail.com>
2025-03-17 20:39:06 +0200

    add

--- task.cpp
+++ task.cpp
@@ -19,6 +19,27 @@ vector<Tarefas>listaDeTarefas;
 void adicionarTarefas(){
     Tarefas novaTarefa;
     novaTarefa.id=listaDeTarefas.size()+1;
+
+    cout << "Digite a descrricao da tarefa: ";
+    cin.ignore();//limpa oo buffer do teclado
+    getline(cin,novaTarefa.descricao);
+
+    listaDeTarefas.push_back(novaTarefa);
+    cout <<"tarefa adicionada com sucesso!\n\n";
+}
+
+// funcao para listar todas as tarefas
+void  listarTarefa(){
+    if(listaDeTarefas.empty()){
+        cout << "Nenhuma tarefa registada!\n\n ";
+        return;
+    }
+
+    cout << "===Lista de Tarefas===\n";
+    for(const auto& tarefa:listaDeTarefas){
+        cout<< "ID: "<<tarefa.id<<" - "<<"Descricao: "<<tarefa.descricao<<endl;
+    }
+    cout<<endl;
 }
 
 int maian(){
